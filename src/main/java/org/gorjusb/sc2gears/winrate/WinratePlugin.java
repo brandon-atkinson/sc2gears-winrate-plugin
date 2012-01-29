b475befe8bc7bc6e0d847bcbbde94b5c5d9218df
U-CRACKER\brandon <brandon@CRACKER.(none)>
2012-01-29 17:12:28 -0500

    adding compile plugin target version fleshing out init method

--- src/main/java/org/gorjusb/sc2gears/winrate/WinratePlugin.java
+++ src/main/java/org/gorjusb/sc2gears/winrate/WinratePlugin.java
@@ -1,11 +1,14 @@
 package org.gorjusb.sc2gears.winrate;
 
+import javax.swing.JDialog;
+
 import hu.belicza.andras.sc2gearspluginapi.Configurable;
 import hu.belicza.andras.sc2gearspluginapi.GeneralServices;
 import hu.belicza.andras.sc2gearspluginapi.Plugin;
 import hu.belicza.andras.sc2gearspluginapi.PluginDescriptor;
 import hu.belicza.andras.sc2gearspluginapi.PluginServices;
 import hu.belicza.andras.sc2gearspluginapi.SettingsControl;
+import hu.belicza.andras.sc2gearspluginapi.api.listener.ReplayAutosaveListener;
 
 public class WinratePlugin implements Plugin, Configurable {
 	
@@ -13,25 +16,25 @@ public class WinratePlugin implements Plugin, Configurable {
 	private PluginServices pluginServces;
 	private GeneralServices generalServices;
 	
+	private ReplayAutosaveListener replayListener;
+	private JDialog winrateDialog;
+	
 	public SettingsControl getSettingsControl() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public boolean isActionRequired() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public void destroy() {
-		// TODO Auto-generated method stub
-
 	}
 
-	public void init(PluginDescriptor arg0, PluginServices arg1,
-			GeneralServices arg2) {
-		// TODO Auto-generated method stub
-
+	public void init(PluginDescriptor pluginDescriptor, PluginServices pluginServices,
+			GeneralServices generalServices) {
+		this.pluginDescriptor = pluginDescriptor;
+		this.pluginServces = pluginServices;
+		this.generalServices = generalServices;
 	}
 
 	PluginDescriptor getPluginDescriptor() {
