c77b3393cf7a346faffb5ffcd03b109bb35599e1
Brandon N. Atkinson <brandon.n.atkinson@gmail.com>
2012-01-31 16:53:42 -0500

    added some basic experiment type code for getting familiar with api

--- src/main/java/org/gorjusb/sc2gears/winrate/WinratePlugin.java
+++ src/main/java/org/gorjusb/sc2gears/winrate/WinratePlugin.java
@@ -1,24 +1,51 @@
 package org.gorjusb.sc2gears.winrate;
 
-import javax.swing.JDialog;
-
 import hu.belicza.andras.sc2gearspluginapi.Configurable;
 import hu.belicza.andras.sc2gearspluginapi.GeneralServices;
 import hu.belicza.andras.sc2gearspluginapi.Plugin;
 import hu.belicza.andras.sc2gearspluginapi.PluginDescriptor;
 import hu.belicza.andras.sc2gearspluginapi.PluginServices;
 import hu.belicza.andras.sc2gearspluginapi.SettingsControl;
+import hu.belicza.andras.sc2gearspluginapi.api.ReplayFactoryApi.ReplayContent;
 import hu.belicza.andras.sc2gearspluginapi.api.listener.ReplayAutosaveListener;
+import hu.belicza.andras.sc2gearspluginapi.api.sc2replay.IReplay;
+
+import java.awt.Window;
+import java.io.File;
+import java.util.EnumSet;
 
 public class WinratePlugin implements Plugin, Configurable {
-	
+
 	private PluginDescriptor pluginDescriptor;
 	private PluginServices pluginServces;
 	private GeneralServices generalServices;
-	
+
 	private ReplayAutosaveListener replayListener;
-	private JDialog winrateDialog;
-	
+	private Window winrateWindow;
+
+	private WinrateModel winrateModel = new WinrateModel();
+
+	class WinrateReplayAutoSaveListener implements ReplayAutosaveListener {
+		@Override
+		public void replayAutosaved(File autosavedReplayFile,
+				File originalReplayFile) {
+			IReplay replay = generalServices.getReplayFactoryApi().getReplay(
+					autosavedReplayFile.getAbsolutePath(),
+					EnumSet.allOf(ReplayContent.class));
+			
+			for (String winner : replay.getWinnerNames().split(",")) {
+				System.out.println(winner.trim());
+			}
+			
+			for (String player : replay.getPlayerNames().split(",")) {
+				System.out.println(player.trim());
+			}
+		}
+	}
+
+	public WinratePlugin() {
+	}
+
 	public SettingsControl getSettingsControl() {
 		return null;
 	}
@@ -28,13 +55,21 @@ public class WinratePlugin implements Plugin, Configurable {
 	}
 
 	public void destroy() {
+		/*
+		 * unregister our replay listener, because we play nice
+		 */
+		generalServices.getCallbackApi().removeReplayAutosaveListener(
+				replayListener);
 	}
 
-	public void init(PluginDescriptor pluginDescriptor, PluginServices pluginServices,
-			GeneralServices generalServices) {
+	public void init(PluginDescriptor pluginDescriptor,
+			PluginServices pluginServices, GeneralServices generalServices) {
 		this.pluginDescriptor = pluginDescriptor;
 		this.pluginServces = pluginServices;
 		this.generalServices = generalServices;
+
+		generalServices.getCallbackApi().addReplayAutosaveListener(
+				new WinrateReplayAutoSaveListener());
 	}
 
 	PluginDescriptor getPluginDescriptor() {
