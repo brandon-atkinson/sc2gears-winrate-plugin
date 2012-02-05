bd4b0014ba0ebdf09f3e15dfc4c8eca729e0e860
Brandon N. Atkinson <brandon.n.atkinson@gmail.com>
2012-02-04 23:56:11 -0500

    Adding WinrateSettingsControl

---
+++ src/main/java/org/gorjusb/sc2gears/winrate/WinrateSettingsControl.java
@@ -0,0 +1,29 @@
+package org.gorjusb.sc2gears.winrate;
+
+import hu.belicza.andras.sc2gearspluginapi.SettingsControl;
+
+import java.awt.Container;
+
+import javax.swing.JDialog;
+
+public class WinrateSettingsControl implements SettingsControl {
+	public WinrateSettingsControl() {
+	}
+
+	public Container getEditorPanel() {
+		return null;
+	}
+
+	public void onCancelButtonPressed() {
+		// TODO 
+	}
+
+	public void onOkButtonPressed() {
+		// TODO Auto-generated method stub
+	}
+
+	public void receiveSettingsDialog(JDialog arg0) {
+		// TODO Auto-generated method stub
+	}
+
+}
