dd85e0ee89af1aad7d377823a2ac156c433136b6
Brandon N. Atkinson <brandon.n.atkinson@gmail.com>
2012-02-05 22:30:28 -0500

    reworking model calculator classes.k

--- src/main/java/org/gorjusb/sc2gears/winrate/WinrateCalculationStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.gorjusb.sc2gears.winrate;
-
-/**
- * @author brandon
- *
- */
-public interface WinrateCalculationStrategy {
-	int calculate(int gamesWon, int gamesLost, int gamesDrawn); 
-}
